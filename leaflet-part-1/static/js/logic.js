a8a294d2c3a3504ee361d289b392e7eadc24cc22
ggiudice98 <giancarlogiudice@live.com>
2024-01-09 18:37:16 -0500

    final commit

--- leaflet-part-1/static/js/logic.js
+++ leaflet-part-1/static/js/logic.js
@@ -1,6 +1,6 @@
 // Siginificant earthquake past 7 days
 
-url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_week.geojson"
+url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson"
 
 // Create Map
 
@@ -23,42 +23,32 @@ d3.json(url).then(function(Data) {
     pointToLayer: function (Data, latlng) {
         return L.circleMarker(latlng,{
             radius: (Data.properties.mag) * 5,
-            fillcolor: chooseColor(Data.geometry.coordinates[2]),
-            color: "black",
+            fillColor: chooseColor(Data.geometry.coordinates[2]),
+            color: chooseColor,
             weight: 1,
-            fillopacity: 0.8
-        });
+            opacity: 10000,
+            fillOpacity: 1000   });
     },
 
-    style: mapStyle,
 }).addTo(map);
 
-// First attribute: Color according to depth
+//according to depth
 
 function chooseColor(depth) {
-    if (depth < -10) return "rgb(163,246,0";
-    else if (depth < 10) return "rgb(230,244,0";
-    else if (depth <30) return "rgb(247,219,17";
-    else if (depth <70) return "rgb(253,183,42";
-    else if (depth <90) return "rgb(252,155,75";
-    else return "rgb(225,87,91)";
+    if (depth < -10) return "lightgreen";
+    else if (depth < 10) return "green";
+    else if (depth <30) return "yellow";
+    else if (depth <70) return "orange";
+    else if (depth <90) return "pink";
+    else return "red";
 };
-
 
 
 // Adding a legend
 
-var legend = L.control({position: "bottomright"});
+var legend = L.control({ position: "bottomright" });
 
-    legend.onAdd = function(Map) {
-        let div = L.DomUtil.create("div", "info legend"),
-        depth = [-10,10,30,50,70,90];
-    div.innerhtml = "<strong>Depth (km)</strong><br>";
-
-    return div;
-};
-
-legend.addTo(map)
+    legend.addTo(map)
 
 });
 
